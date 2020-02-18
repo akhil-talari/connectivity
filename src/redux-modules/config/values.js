728813a7d9845628740f30c5241e6eb135d12fae
anemilid <31550136+anemilid@users.noreply.github.com>
2020-02-17 22:17:43 -0600

    end point change

--- src/redux-modules/config/values.js
+++ src/redux-modules/config/values.js
@@ -2,13 +2,13 @@
 export const appVersion = '1.0';
 /////////////////// Generic APIs //////////////////////////
 export const getServiceRequestNumber =
-  'http://ec2-3-89-131-167.compute-1.amazonaws.com:8080/api/user';
+  'http://ec2-52-206-53-178.compute-1.amazonaws.com:8080/api/user';
 
 export const createUserURI =
-  'http://ec2-3-89-131-167.compute-1.amazonaws.com:8080/api/user/create_user_form';
+  'http://ec2-52-206-53-178.compute-1.amazonaws.com:8080/api/user/create_user_form';
 
 export const sendOTPURI =
-  'http://ec2-3-89-131-167.compute-1.amazonaws.com:8080/api/user/emailauthentication';
+  'http://ec2-52-206-53-178.compute-1.amazonaws.com:8080/api/user/emailauthentication';
 
 export const authenticateURI =
-  'http://ec2-3-89-131-167.compute-1.amazonaws.com:8080/api/user/loginauthentication';
+  'http://ec2-52-206-53-178.compute-1.amazonaws.com:8080/api/user/loginauthentication';
